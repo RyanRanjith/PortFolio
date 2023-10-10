e335b231c49e6037fa150e9d2914428dc7813d6a
Ranjith Gopinath <kingranjith4@gmail.com>
2023-10-09 21:34:19 -0700

    10-09-2023

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
+import Works from "./components/Works/works";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
@@ -8,7 +11,10 @@ function App() {
       <Navbar/>
       <Intro/>
       <Skills/>
-      
+      <Works/>
+      <Contact/>
+      <Footer/>
+
     </div>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const footer = () => {
+  return (
+   <footer className='footer'>
+    Copyright &#169; 2023 Ranjith Gopinath. All right received
+   </footer>
+  )
+}
+
+export default footer

079850970d809dfa705bfeb46b850ac6d90225e6
Ranjith Gopinath <kingranjith4@gmail.com>
2023-10-11 12:11:19 -0700

    Deployed website

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact.css';
 import walmart from '../../assets/walmart.png';
 import Adobe from '../../assets/adobe.png';
@@ -8,8 +8,23 @@ import FacebookIcon from '../../assets/facebook-icon.png';
 import TwitterIcon from '../../assets/twitter.png';
 import YouTubeIcon from '../../assets/youtube.png';
 import InstagramIcon from '../../assets/instagram.png';
+import emailjs from '@emailjs/browser';
 
-const contact = () => {
+const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_vvdmmxw', 'template_du6t41u', form.current, 'BdPQJtfoJzixs57m8')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+          alert('Email Sent ')
+      }, (error) => {
+           console.log(error.text);
+        });
+    };
+
   return (
     <section id="contactPage">
         <div id="clients">
@@ -25,19 +40,19 @@ const contact = () => {
        </div>
         </div>
         <div id="contact">
-       <h1 className="contactPageTitle">Contact Me</h1>
-       <span className="contactDesc">Please fill out the form below to discuss any work oppurtunities</span>
+         <h1 className="contactPageTitle">Contact Me</h1>
+         <span className="contactDesc">Please fill out the form below to discuss any work oppurtunities</span>
        
-       <form className='contactForm'>
-        <input type="text" className="name" placeholder='Your Name' />
-        <input type="email" className="email" placeholder='Your Email' />
-        <textarea className="msg"   rows="5" placeholder='Your Message'></textarea>
-        <button type = 'submit' value='send' className="submitBtn">Submit</button>
-        <div className="links">
-          <img src={FacebookIcon} alt="Facebook" className="link" />
-          <img src={TwitterIcon} alt="twitter" className="link" />
-          <img src={YouTubeIcon} alt="Youtube" className="link" />
-          <img src={InstagramIcon} alt="Instagram" className="link" />
+         <form className='contactForm' ref={form} onSubmit={sendEmail}>
+            <input type="text" className="name" placeholder='Your Name' name='your_name' />
+            <input type="email" className="email" placeholder='Your Email' name='your_email' />
+            <textarea className="msg" name='message'  rows="5" placeholder='Your Message'></textarea>
+            <button type='submit' value='Send' className="submitBtn">Submit</button>
+            <div className="links">
+                 <img src={FacebookIcon} alt="Facebook" className="link" />
+                 <img src={TwitterIcon} alt="twitter" className="link" />
+                 <img src={YouTubeIcon} alt="Youtube" className="link" />
+                 <img src={InstagramIcon} alt="Instagram" className="link" />
         </div>
        </form>
         </div>
@@ -45,4 +60,4 @@ const contact = () => {
   )
 }
 
-export default contact
+export default Contact

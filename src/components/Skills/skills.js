4a3b876b0946acad8068a4952577b35ba599319f
Ranjith Gopinath <kingranjith4@gmail.com>
2023-10-14 11:43:27 -0700

    10-14-23

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -8,27 +8,27 @@ const skills = () => {
   return (
     <section id='skills'>
         <span className="skillTitle"> What I do</span>
-        <span className="skillDesc">I am a skilled and passionate web designer with experience in creating visually appealing and user-friendly  websites. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML, CSS, and JavaScript as well as design software such as Adobe Photoshop and Illustrator</span>
+        <span className="skillDesc"> This is model website creation. I created with the help of JavaScript, CSS and HTML and used ReactEmail Js for Email Communication</span>
         <div className="skillBars">
             <div className="skillBar">
                 <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>UI/UX Design</h2>
-                    <p>This is a demo text, you can write your own content here</p>
+                    <p>This is a demo text.</p>
                 </div>
             </div>
             <div className="skillBar">
                 <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>Website Design</h2>
-                    <p>This demo text can be changed while making the production ready website.</p>
+                    <p>This demo text.</p>
                 </div>
             </div>
             <div className="skillBar">
                 <img src={AppDesign} alt="AppDesign" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>App Design</h2>
-                    <p>You can write text related to mobile app development.</p>
+                    <p>Demo Text.</p>
                 </div>
             </div>
             

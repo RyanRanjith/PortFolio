df74097b12ed943d528814874cd3a99404c60d31
Ranjith Gopinath <kingranjith4@gmail.com>
2023-10-25 09:54:07 -0700

    10-25-23

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -14,15 +14,17 @@ const skills = () => {
                 <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>Bachelors in Electronics and Communication<br></br> (2016-2020)</h2>
-
-                    <p>Anna Univeristy, Chennai, India.</p>
+                  <span className='b1'>Anna Univeristy, Chennai, India.</span>
+                    {/* <p>Anna Univeristy, Chennai, India.</p> */}
                 </div>
             </div>
             <div className="skillBar">
                 <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>Masters in Computer Science<br></br>(2022)</h2>
-                    <p>California State University, Dominguez Hills, California,US.</p>
+                    <span className='m1'>California State University, Dominguez Hills, California,US.</span>
+                   {/* <p >California State University, Dominguez Hills, California,US.</p> */}
+
                 </div>
             </div>
             

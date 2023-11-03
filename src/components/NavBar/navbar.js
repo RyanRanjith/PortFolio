c9ed66426193513544ddf61ab1b312147da34c7c
Ranjith Gopinath <kingranjith4@gmail.com>
2023-11-03 14:58:25 -0700

    j

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
      <Link activeClass='active'to='skills' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Education</Link>
      <Link activeClass='active'to='works' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Experience</Link>
      <Link activeClass='active'to='clients' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Clients</Link>
-     <Link activeClass='active'to='contact' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Contact</Link>
+     <Link activeClass='active'to='contacT' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Contact</Link>
     </div>
     </nav>
   )
